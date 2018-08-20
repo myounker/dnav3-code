c54ae1c52ca2287b2ee67ba34a79c060916dad08
Mike Younkers <msyounk@gmail.com>
2018-08-19 21:59:11 -0400

    Hands on lab for python part 1

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,24 +7,38 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
-
+print(type(pi))
+print(pi)
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
+if i < 50:
+    print('i is less than 50! i = ', i)
+elif i == 50:
+    print ('i is 50! i = ', i)
+else:
+    print ('i is greater than 50! i = ', i)
 
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+    print('orange was selected and this is the color orange')
+elif picked_fruit == 'strawberry':
+    print('strawberry was selected and this is the color red')
+elif picked_fruit == 'banana':
+    print('banana was selected and this is the collor yellow')
+else:
+    print ('none of the 3 fruits matched. error!')
 
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
-
+def mult(num1, num2):
+    result = num1 * num2
+    return result
 
 # TODO: Now call the function a few times to calculate the following answers
-
-print("12 x 96 =",)
-
-print("48 x 17 =",)
-
-print("196523 x 87323 =",)
+print("12 x 96 =", mult(12, 96))
+print("48 x 17 =", mult(48, 17))
+print("196523 x 87323 =", mult(196523, 87323))
